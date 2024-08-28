9712098d9a3de5affbeed680e46afeb35c6949a7
tenlive1 <nguyenperry99@gmail.com>
2024-08-28 15:00:52 -0400

    figure out how to add the number to the cart

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -70,7 +70,7 @@ const CartItem = ({ onContinueShopping }) => {
           </div>
         ))}
       </div>
-      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'>0</div>
+      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
