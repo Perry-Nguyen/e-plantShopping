ddca96adc0c2cedb7fa3df82f990014f0e2b29e4
tenlive1 <nguyenperry99@gmail.com>
2024-08-28 17:27:25 -0400

    done with task3

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,6 +4,8 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    totalQuanity: 0,
+
   },
   reducers: {
     addItem: (state, action) => {
@@ -14,17 +16,23 @@ export const CartSlice = createSlice({
         } else {
             state.items.push({ name, image, cost, quantity: 1 });
         }
+        state.totalQuanity++;
     },
     removeItem: (state, action) => {
-        const { name, image, cost } = action.payload;
+        const { name, image, cost, quantity} = action.payload;
         state.items = state.items.filter(item => item.name !== name);
+        state.totalQuanity -= quantity;
+        
     },
     updateQuantity: (state, action) => {
     
         const{name,quantity} = action.payload;
         const itemToUpdate = state.items.find(item => item.name === name);
         if(itemToUpdate){
+            state.totalQuanity -= itemToUpdate.quantity;
             itemToUpdate.quantity = quantity;
+            state.totalQuanity += itemToUpdate.quantity;
+
         }
     
     },
