3fc111a7fa027705d5aed5a51564d8241e50019f
tenlive1 <nguyenperry99@gmail.com>
2024-08-28 07:46:04 -0400

    done with remove

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -2,7 +2,6 @@ import React, { useState,useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addItem, removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
-import ProductList from './ProductList';
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
